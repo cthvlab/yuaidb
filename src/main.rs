c1f8dcc4c24f8ac6b86978e207abb7543c796013
cthvlab <60858283+cthvlab@users.noreply.github.com>
2025-03-13 18:35:31 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,26 @@ use yuaidb::Database;
 async fn main() {
     let db = Database::new("./data", "./config.toml").await;
 
-    // Добавляем пиратов
-    db.start_insert("pirates")
+    db.insert("pirates")
         .values(vec![("pirate_id", "1"), ("name", "Капитан Джек Воробот"), ("ship_id", "101")])
         .execute(&db)
         .await;
 
-    db.start_insert("pirates")
+    db.insert("pirates")
         .values(vec![("pirate_id", "2"), ("name", "Лихой Билл"), ("ship_id", "102")])
         .execute(&db)
         .await;
 
-    // Добавляем корабли
-    db.start_insert("ships")
+    db.insert("ships")
         .values(vec![("ship_id", "101"), ("name", "Чёрная Комета"), ("speed", "0.9c")])
         .execute(&db)
         .await;
 
-    db.start_insert("ships")
+    db.insert("ships")
         .values(vec![("ship_id", "102"), ("name", "Астероидный Шторм"), ("speed", "0.7c")])
         .execute(&db)
         .await;
 
-    // Запрос: "Кто на чём летает?"
     println!("Кто на чём летает:");
     if let Some(rows) = db.select("pirates")
         .alias("p")
@@ -45,9 +42,10 @@ async fn main() {
         }
     }
 
-    // Джек меняет корабль
-    db.start_insert("pirates")
-        .values(vec![("pirate_id", "1"), ("name", "Капитан Джек Воробот"), ("ship_id", "102")])
+    // Обновляем корабль для Джека
+    db.update("pirates")
+        .values(vec![("ship_id", "102")]) // Меняем корабль на "Астероидный Шторм"
+        .where_eq("pirate_id", "1")
         .execute(&db)
         .await;
 
@@ -56,7 +54,7 @@ async fn main() {
         .alias("p")
         .fields(vec!["p.name", "s.name"])
         .join("ships", "s", "s.ship_id", "p.ship_id")
-        .where_clause("p.pirate_id = '1'")
+        .where_eq("p.pirate_id", "1")
         .execute(&db)
         .await
     {
